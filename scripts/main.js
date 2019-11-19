fa39c3a95cee26976cf791cc162665b2055c8068
Gabriel Panetta <ark.gp20@gmail.com>
2019-11-19 11:07:40 -0500

    Complete the game by displaying the match result and a replay button

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Use the transition effect that we just learned to display the computer choice.
+
 function computerPlay() {
     switch (Math.floor(Math.random() * 3)) {
         case 0:
@@ -63,50 +65,76 @@ function playRound(e) {
         }
 
     }
+
+    roundResult();
 }
 
-function game() {
-    let playerPoint = 0;
-    let computerPoint = 0;
-
-    for (let index = 0; index < 5; index++) {
-        const playerChoice = prompt('Choose between rock, paper or scissors');
-        const computerChoice = computerPlay();
-
-        let roundResult = playRound(playerChoice, computerChoice);
-
-        if (roundResult.includes('You win!')) {
-            playerPoint++;
-        } else if (roundResult.includes('You lose!')) {
-            computerPoint++;
-        } else if (roundResult.includes('Tie!')) {
-            playerPoint++;
-            computerPoint++;
+function roundResult() {
+    let playerPoint = 
+        document.querySelector('#player-point span'),
+    computerPoint = 
+        document.querySelector('#computer-point span'),
+    roundNumber = 
+        document.querySelector('#round-number span'),
+    result = document.getElementById('round-result');
+    
+    if (+playerPoint.textContent < 5 && +computerPoint.textContent < 5) {
+        if (result.textContent.includes('You win!')) {
+            playerPoint.textContent = +playerPoint.textContent + 1;
+        } else if (result.textContent.includes('You lose!')) {
+            computerPoint.textContent = +computerPoint.textContent + 1;
+        } else if (result.textContent.includes('Tie!')) {
+            playerPoint.textContent = +playerPoint.textContent + 1;
+            computerPoint.textContent = +computerPoint.textContent + 1;
         }
-
-        console.log(roundResult);
+    
+        roundNumber.textContent = +roundNumber.textContent + 1;
+    } else {
+        gameEnd(playerPoint, computerPoint);
     }
 
-    displayResult(playerPoint, computerPoint);
 }
 
-function displayResult(playerPoint, computerPoint) {
-    if (playerPoint > computerPoint) {
+function gameEnd(playerPoint, computerPoint) {
+    let gameMessage;
+    if (+playerPoint.textContent > +computerPoint.textContent) {
+        gameMessage = 
+        `Congratulations! You won the match with ${+playerPoint.textContent} 
+        points against ${+computerPoint.textContent} points.`;
+    } else if (+playerPoint.textContent < +computerPoint.textContent) {
+        gameMessage = 
+        `Oh no! You lost the match with ${+playerPoint.textContent} 
+        points against ${+computerPoint.textContent} points.`;
 
-        console.log(`Congratulations! You won the match with ${playerPoint} 
-        points against ${computerPoint} points.`);
-
-    } else if (playerPoint < computerPoint) {
-
-        console.log(`Oh no! You lost the match with ${playerPoint} points
-        against ${computerPoint} points.`);
-
-    } else if (playerPoint === computerPoint) {
-
-        console.log(`It's a tie! You both had ${playerPoint} points.`);
-
+    } else if (+playerPoint.textContent === +computerPoint.textContent) {
+        gameMessage = 
+        `It's a tie! You both had ${+playerPoint.textContent} points.`;
     }
+
+    gameResultDisplay(gameMessage);
 }
+
+function gameResultDisplay(gameMessage) {
+    const buttonContainer = document.getElementById("button-container");
+    while (buttonContainer.firstChild) {
+        buttonContainer.removeChild(buttonContainer.firstChild);
+    }
+
+    const message = document.getElementById('round-result');
+    message.textContent = gameMessage;
+
+    const replayButton = document.createElement('button');
+    replayButton.textContent = 'Replay';
+    replayButton.addEventListener('click', gameReset);
+
+    buttonContainer.appendChild(replayButton);
+
+}
+
+function gameReset() {
+    location.reload();
+}
+
 
 const gameButtons = document.querySelectorAll('#button-container');
 
